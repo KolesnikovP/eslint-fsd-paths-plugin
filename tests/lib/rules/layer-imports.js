461fb9a2303f4edbf145788043e0eb1b04f3c1b3
petrkolesnikov <ptrklsnkv@gmail.com>
2024-04-21 21:55:18 -0500

    beggining of the new rule

---
+++ tests/lib/rules/layer-imports.js
@@ -0,0 +1,31 @@
+/**
+ * @fileoverview inablity to import files from upper layers
+ * @author petr
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/layer-imports"),
+  RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester();
+ruleTester.run("layer-imports", rule, {
+  valid: [
+    // give me some code that won't trigger a warning
+  ],
+
+  invalid: [
+    {
+      code: "",
+      errors: [{ message: "Fill me in.", type: "Me too" }],
+    },
+  ],
+});
